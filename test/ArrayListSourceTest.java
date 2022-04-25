eb138adca1fa892c39cc0ebe3f9b2ac82ddb9951
dove bai <dove.bai@client.com.au>
2022-04-25 21:49:48 +0800

    Array List Source

--- test/ArrayListSourceTest.java
+++ test/ArrayListSourceTest.java
@@ -18,16 +18,17 @@ public class ArrayListSourceTest {
         // 不要使用0作为构造参数构建ArrayList
         // 那么存在这个构造函数的意义是什么呢？在我们确定容量且不会进行增删的时候使用
         ArrayList<Object> objects = new ArrayList<>(0);
-        objects.add("a");
-        objects.add("b");
-        objects.add("c");
-        objects.add("d");
-        objects.add("e");
-        objects.add("f");
-        objects.add("g");
-        objects.add("h");
-        objects.add("i");
-        objects.add("j");
+                       // minCapacity（size+1）minCapacity-elementData.length>0扩容  计算newCapacity      elementData最终容量（两个判断，见grow方法）
+        objects.add("a"); // minCapacity=1   1-0(elementData.length)>0             0+（0/2）=0           1（扩容后）
+        objects.add("b"); // minCapacity=2   2-1(elementData.length)>0             1+（1/2）=1           2（扩容后）
+        objects.add("c"); // minCapacity=3   3-2(elementData.length)>0             2+（2/2）=3           3（扩容后）
+        objects.add("d"); // minCapacity=4   4-3(elementData.length)>0             3+（3/2）=4           4（扩容后）
+        objects.add("e"); // minCapacity=5   5-4(elementData.length)>0             4+（4/2）=6           6（扩容后）
+        objects.add("f"); // minCapacity=6   6-6(elementData.length)！>0           不扩容
+        objects.add("g"); // minCapacity=7   7-6(elementData.length)>0             6+（6/2）=9           9（扩容后）
+        objects.add("h"); // minCapacity=8   8-9(elementData.length)！>0           不扩容
+        objects.add("i"); // minCapacity=9   9-9(elementData.length)！>0           不扩容
+        objects.add("j"); // minCapacity=10  10-9(elementData.length)>0            9+（9/2）=13          13（扩容后）
         System.out.println(objects);
 
         List<Object> objects1 = Arrays.asList();
