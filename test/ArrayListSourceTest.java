4b94e3e216c32a73006ab0118e92fd92f59205ab
dove bai <dove.bai@client.com.au>
2022-04-26 09:14:12 +0800

    Array List Source

--- test/ArrayListSourceTest.java
+++ test/ArrayListSourceTest.java
@@ -18,7 +18,7 @@ public class ArrayListSourceTest {
         // 不要使用0作为构造参数构建ArrayList
         // 那么存在这个构造函数的意义是什么呢？在我们确定容量且不会进行增删的时候使用
         ArrayList<Object> objects = new ArrayList<>(0);
-                       // minCapacity（size+1）minCapacity-elementData.length>0扩容  计算newCapacity      elementData最终容量（两个判断，见grow方法）
+        // minCapacity（size+1）minCapacity-elementData.length>0扩容  计算newCapacity      elementData最终容量（两个判断，见grow方法）
         objects.add("a"); // minCapacity=1   1-0(elementData.length)>0             0+（0/2）=0           1（扩容后）
         objects.add("b"); // minCapacity=2   2-1(elementData.length)>0             1+（1/2）=1           2（扩容后）
         objects.add("c"); // minCapacity=3   3-2(elementData.length)>0             2+（2/2）=3           3（扩容后）
@@ -38,5 +38,30 @@ public class ArrayListSourceTest {
 
         List<Object> objects2 = Collections.emptyList();
         // if else场景直接返回一个空数组使用，不会创建新空间（上面的都会使用new关键字开辟内存空间）
+
+        // 性能最高的拷贝
+        // 方法实现：从源数组的指定位置开始复制指定长度的元素到目标数组的指定位置中
+        // 第一个参数就是源数组，第二个参数是要复制的源数组中的起始位置，第三个参数是目标数组，第四个参数是要复制到的目标数组的起始位置，第五个参数是要复制的元素的长度。
+        String[] sourceArr = new String[1];
+        sourceArr[0] = "a";
+        String[] targetArr = new String[5];
+        System.arraycopy(sourceArr, 0, targetArr, 2, 1);
+        System.out.println(Arrays.toString(targetArr));
+
+        // objects2.add(index , element); 会导致元素后移
+        // remove(element); 会导致元素前移
+
+
+        ArrayList<String> objects3 = new ArrayList<>();
+        objects3.add("a");
+        objects3.clear();
+        objects3.add("e");
+        System.out.println("请问经过几次扩容？");
+        // clear重制了size，但是没有重制elementData数组的长度，所以以上逻辑只会扩容一次
+
+        ArrayList<Object> objects4 = new ArrayList<>();
+        ArrayList<Object> objects5 = new ArrayList<>();
+        System.out.println(objects5.addAll(objects4));// false
+        // 一定要慎用addAll的返回值
     }
 }
